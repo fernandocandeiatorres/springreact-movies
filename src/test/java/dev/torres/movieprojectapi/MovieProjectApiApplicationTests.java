6f80cdc6d727dfce057b5c6caa2bb6e2f74e2db3
fernandocandeiatorres <fernandotorrescandeia@gmail.com>
2023-06-17 19:51:09 -0300

    first commit

---
+++ src/test/java/dev/torres/movieprojectapi/MovieProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.torres.movieprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieProjectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
